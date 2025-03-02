75074619fbd1f14126ad0227409d95277bf8ff21
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-03-02 14:21:07 +0200

    Lesson 10 homework fixes of logic

--- HomeWork/Lesson 10/js/controller.js
+++ HomeWork/Lesson 10/js/controller.js
@@ -1,61 +1,47 @@
 let editId = null
 
 /////// form elements ///////
+const form = document.querySelector('#book-form')
+
+/////// form elements ///////
 const bookTitle = document.querySelector('#book-title')
 const bookAuthor = document.querySelector('#book-author')
 const bookYear = document.querySelector('#book-year')
 const bookGenre = document.querySelector('#book-genre')
-const submitButton = document.querySelector('#submit')
+const elementsForValidation = document.querySelectorAll('[data-validate="true"]')
 
 /////// books list container ///////
 const bookListContainer = document.querySelector('#book-list')
 
 /////// event listeners ///////
-submitButton.addEventListener('click', event => {
-    event.preventDefault()
-
-    /////// validate required ///////
-    validate(bookTitle, 'Введіть назву книги', () => checkRequired(bookTitle))
-    validate(bookAuthor, 'Введіть автора', () => checkRequired(bookAuthor))
-    validate(bookYear, 'Введіть рік видання', () => checkRequired(bookYear))
-    validate(bookGenre, 'Введіть жанр', () => checkRequired(bookGenre))
-
-    /////// validate year ///////
-    validate(bookYear, 'Введене значення не відповідає шаблону', () => checkPattern(bookYear))
-    validate(bookYear, 'Значення нижче за мінімальне', () => checkMinValue(bookYear))
-    validate(bookYear, 'Значення вище за максимальне', () => checkMaxValue(bookYear))
-
-    /////// validate book title duplicates ///////
-    validate(bookTitle, 'Книга вже є у списку', () => checkDuplicates(bookTitle))
-
-    const invalidElements = document.querySelectorAll('.invalid')
-    if (invalidElements.length === 0) {
-        const book = {
-            title: bookTitle.value,
-            author: bookAuthor.value,
-            year: bookYear.value,
-            genre: bookGenre.value
-        }
-        if (editId) {
-            book.id = editId
-            data.update(editId, book)
-            editId = null
-        } else {
-            data.add(book)
-        }
-        clearForm()
-        render()
-    }
+form.addEventListener('submit', event => {
+    // event.preventDefault()
+    //
+    // const invalidElements = document.querySelectorAll('.invalid')
+    // if (invalidElements.length === 0) {
+    //     const book = {
+    //         title: bookTitle.value,
+    //         author: bookAuthor.value,
+    //         year: bookYear.value,
+    //         genre: bookGenre.value
+    //     }
+    //     if (editId) {
+    //         book.id = editId
+    //         data.update(editId, book)
+    //         editId = null
+    //     } else {
+    //         data.add(book)
+    //     }
+    //     clearForm()
+    //     render()
+    // }
 })
 
-bookTitle.addEventListener('change', () => validate(bookTitle, 'Введіть назву книги', () => checkRequired(bookTitle)))
-bookAuthor.addEventListener('change', () => validate(bookAuthor, 'Введіть автора', () => checkRequired(bookAuthor)))
-bookYear.addEventListener('change', () => validate(bookYear, 'Введіть рік видання', () => checkRequired(bookYear)))
-bookGenre.addEventListener('change', () => validate(bookGenre, 'Введіть жанр', () => checkRequired(bookGenre)))
-bookYear.addEventListener('change', () => validate(bookYear, 'Введене значення не відповідає шаблону', () => checkPattern(bookYear)))
-bookYear.addEventListener('change', () => validate(bookYear, 'Значення нижче за мінімальне', () => checkMinValue(bookYear)))
-bookYear.addEventListener('change', () => validate(bookYear, 'Значення вище за максимальне', () => checkMaxValue(bookYear)))
-bookTitle.addEventListener('change', () => validate(bookTitle, 'Книга вже є у списку', () => checkDuplicates(bookTitle)))
+elementsForValidation.forEach(element => {
+    element.addEventListener('change', onChangeHandler)
+})
+
+form.addEventListener('submit', onSubmitHandler)
 
 render()
 
@@ -86,53 +72,46 @@ function editBook(id) {
     bookGenre.value = book.genre
 }
 
-function validate(element, errorMessage, predicate) {
-    const errorLabel = document.querySelector(`#${element.dataset.errorLabel}`)
-    errorLabel.textContent = errorMessage
-
-    element.classList.remove('valid')
-    element.classList.remove('invalid')
-    errorLabel.style.display = 'none'
-
-    if (typeof predicate === 'function' && predicate()) {
-        element.classList.add('valid')
-    } else {
-        element.classList.add('invalid')
-        errorLabel.style.display = 'inline'
+function validate(element) {
+    for (let key in validators) {
+        if (validators.hasOwnProperty(key) && typeof validators[key] === 'object') {
+            if (element.dataset.hasOwnProperty(key)) {
+                const validator = validators[key]
+                const isValid = validators.validate(element, validator.errorMessage, validator.isValid)
+                if (!isValid) return
+            }
+        }
     }
 }
 
-function checkRequired(element) {
-    const required = element.dataset.required === 'true'
-    if (!required) return true
-
-    return element.value.trim().length > 0
+function onChangeHandler(event) {
+    const element = event.target
+    validate(element)
 }
 
-function checkPattern(element) {
-    const value = element.value
-    const regex = new RegExp(element.dataset.pattern)
-    return regex.test(value)
-}
+function onSubmitHandler(event) {
+    elementsForValidation.forEach(element => {
+        validate(element)
+    })
 
-function checkDuplicates(element) {
-    const duplicatesAllowed = element.dataset.duplicatesAllowed === 'true'
-    if (duplicatesAllowed) return true
+    event.preventDefault()
 
-    const property = element.name
-    const value = element.value
-    const isBookInList = data.getBooks().some(book => book[property] === value)
-    return !isBookInList
-}
-
-function checkMinValue(element) {
-    const minValue = +element.dataset.minValue
-    const currentValue = +element.value
-    return currentValue >= minValue
-}
-
-function checkMaxValue(element) {
-    const maxValue = +element.dataset.maxValue
-    const currentValue = +element.value
-    return currentValue <= maxValue
+    const invalidElements = document.querySelectorAll('.invalid')
+    if (invalidElements.length === 0) {
+        const book = {
+            title: bookTitle.value,
+            author: bookAuthor.value,
+            year: bookYear.value,
+            genre: bookGenre.value
+        }
+        if (editId) {
+            book.id = editId
+            data.update(editId, book)
+            editId = null
+        } else {
+            data.add(book)
+        }
+        clearForm()
+        render()
+    }
 }
--- HomeWork/Lesson 10/js/validators.js
+++ HomeWork/Lesson 10/js/validators.js
@@ -2,11 +2,9 @@ const validators = {}
 
 validators.required = {
     isValid: function(element) {
-        const required = element.dataset.required === 'true'
-        if (!required) return true
-
         return element.value.trim().length > 0
-    }
+    },
+    errorMessage: 'Значення не може бути порожнім'
 }
 
 validators.pattern = {
@@ -14,19 +12,26 @@ validators.pattern = {
         const value = element.value
         const regex = new RegExp(element.dataset.pattern)
         return regex.test(value)
-    }
+    },
+    errorMessage: 'Значення не відповідає шаблону'
 }
 
-validators.duplicales = {
+validators.noDuplicates = {
     isValid: function(element) {
-        const duplicatesAllowed = element.dataset.duplicatesAllowed === 'true'
-        if (duplicatesAllowed) return true
-
         const property = element.name
         const value = element.value
         const isBookInList = data.getBooks().some(book => book[property] === value)
         return !isBookInList
-    }
+    },
+    errorMessage: 'Значення має бути унікальним'
+}
+
+validators.number = {
+    isValid: function(element) {
+        const value = element.value
+        return  Number.isInteger(+value)
+    },
+    errorMessage: 'Значення має бути цілим числом'
 }
 
 validators.minValue = {
@@ -34,7 +39,8 @@ validators.minValue = {
         const minValue = +element.dataset.minValue
         const currentValue = +element.value
         return currentValue >= minValue
-    }
+    },
+    errorMessage: 'Значення менше за допустиме'
 }
 
 validators.maxValue = {
@@ -42,5 +48,24 @@ validators.maxValue = {
         const maxValue = +element.dataset.maxValue
         const currentValue = +element.value
         return currentValue <= maxValue
+    },
+    errorMessage: 'Значення більше за допустиме'
+}
+
+validators.validate = function(element, errorMessage, validator) {
+    const errorLabel = document.querySelector(`#${element.dataset.errorLabel}`)
+    errorLabel.textContent = errorMessage
+
+    element.classList.remove('valid')
+    element.classList.remove('invalid')
+    errorLabel.style.display = 'none'
+
+    if (typeof validator === 'function' && validator(element)) {
+        element.classList.add('valid')
+        return true
+    } else {
+        element.classList.add('invalid')
+        errorLabel.style.display = 'inline'
+        return false
     }
 }
